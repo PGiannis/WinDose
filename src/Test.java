981ec96d3dfe4df8f92199d6aebd0af5dffdeadd
PGiannis <t8180196@aueb.gr>
2020-11-27 13:37:43 +0200

    Turn first registration's page into GUI

--- src/Test.java
+++ src/Test.java
@@ -1,23 +1,130 @@
-import java.util.*;
 
-public class Test {
-	
-	/**The method diagnosis will be the first stage in the choice number 1 of the program
-	 * where the user will insert the diagnosis of a person */
-	
-	public static void diagnosis() {
-		try {
-			System.out.println("Is the person positive to the virus? (true/false)");
-			Scanner sc = new Scanner(System.in);
-			boolean result = sc.nextBoolean();
-			if (result == true) {
-				System.out.println("Positive");
-		} 	else if  (result == false) {
-				System.out.println("Negative");
-		}
-		} catch (InputMismatchException e) {
-			System.out.println("Invalid input");
-		}
+	import javax.swing.*; 
+import java.awt.*; 
+import java.awt.event.*; 
+
+class First_page 
+	extends JFrame
+	implements ActionListener {
+		
+		/* Components of the form*/
+	    private Container c; 
+	    private JLabel title;
+	    private JLabel diagnosis;
+	    private JRadioButton positive;
+	    private JRadioButton negative;
+	    private ButtonGroup gengp; 
+	    private JButton sub;
+	    private JButton back;
+	    private JTextArea tout; 
+	    private JLabel res; 
+	    private JTextArea resadd; 
+	    
+	    public First_page() {
+	    	setTitle("Registration Form"); 
+	        setBounds(300, 90, 900, 600); 
+	        setDefaultCloseOperation(EXIT_ON_CLOSE); 
+	        setResizable(false); 
+	  
+	        c = getContentPane(); 
+	        c.setLayout(null); 
+	  
+	        /*page title*/
+	        title = new JLabel("Registration Form"); 
+	        title.setFont(new Font("Arial", Font.PLAIN, 30)); 
+	        title.setSize(300, 30); 
+	        title.setLocation(300, 30); 
+	        c.add(title); 
+	        
+	        /*Choice name*/
+	        diagnosis = new JLabel ("Diagnosis");
+	        diagnosis.setFont(new Font("Arial", Font.PLAIN, 20)); 
+	        diagnosis.setSize(100, 20); 
+	        diagnosis.setLocation(100, 200); 
+	        c.add(diagnosis); 
+	  
+	        /*positive box*/
+	        positive = new JRadioButton("Positive"); 
+	        positive.setFont(new Font("Arial", Font.PLAIN, 15)); 
+	        positive.setSelected(true); 
+	        positive.setSize(100, 60); 
+	        positive.setLocation(100, 250); 
+	        c.add(positive); 
+	  
+	        /*negative box*/
+	        negative = new JRadioButton("Negative"); 
+	        negative.setFont(new Font("Arial", Font.PLAIN, 15)); 
+	        negative.setSelected(false); 
+	        negative.setSize(100, 60); 
+	        negative.setLocation(100, 300); 
+	        c.add(negative); 
+	  
+	        /*shows that only one option is permitted*/
+	        gengp = new ButtonGroup(); 
+	        gengp.add(positive); 
+	        gengp.add(negative); 
+	        
+	        /*submit button*/
+	        sub = new JButton("Submit"); 
+	        sub.setFont(new Font("Arial", Font.PLAIN, 15)); 
+	        sub.setSize(80, 20); 
+	        sub.setLocation(80, 450); 
+	        sub.addActionListener(this); 
+	        c.add(sub); 
+	        
+	        /*back button*/
+	        back = new JButton("Back"); 
+	        back.setFont(new Font("Arial", Font.PLAIN, 15)); 
+	        back.setSize(80, 20); 
+	        back.setLocation(200, 450); 
+	        back.addActionListener(this); 
+	        c.add(back); 
+	        
+	        tout = new JTextArea(); 
+	        tout.setFont(new Font("Arial", Font.PLAIN, 15)); 
+	        tout.setSize(300, 400); 
+	        tout.setLocation(500, 100); 
+	        tout.setLineWrap(true); 
+	        tout.setEditable(false); 
+	        c.add(tout); 
+	  
+	        res = new JLabel(""); 
+	        res.setFont(new Font("Arial", Font.PLAIN, 20)); 
+	        res.setSize(500, 25); 
+	        res.setLocation(100, 500); 
+	        c.add(res); 
+	  
+	        resadd = new JTextArea(); 
+	        resadd.setFont(new Font("Arial", Font.PLAIN, 15)); 
+	        resadd.setSize(200, 75); 
+	        resadd.setLocation(580, 175); 
+	        resadd.setLineWrap(true); 
+	        c.add(resadd); 
+	  
+	        setVisible(true); 
+	        
+	    } 
+	    
+	    public void actionPerformed(ActionEvent e) 
+	    { 
+	        if (e.getSource() == sub) { 
+	            
+	                String data1; 
+	                
+	                if (positive.isSelected()) 
+	                    data1 = "Diagnosis : Positive" + "\n"; 
+	                else
+	                    data1 = "Diagnosis : Negative" + "\n"; 
+	                
+	               
+	                tout.setText(data1); 
+	                tout.setEditable(false); 
+	                res.setText("Registration Successfully.."); 
+	                
+	     } else if (e.getSource() == back) { 
+	        	tout.setText(""); 
+                resadd.setText(""); 
+                res.setText("Coming Soon!"); 
+	        } 
+	    } 
 	}
-}
-	
