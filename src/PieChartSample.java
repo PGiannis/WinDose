3e780b76e3ab2777b1cd3f25a4b4122d58c8c61e
dimitrakar <dimitrakar@mbp-dimitra.lan>
2020-12-01 22:41:49 +0200

    2o arxeio

---
+++ src/PieChartSample.java
@@ -0,0 +1,48 @@
+package src;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.chart.*;
+import javafx.scene.Group;
+ 
+public class PieChartSample extends Application {
+ 
+	//	public void posPie(int pos, int neg) {
+			
+			
+	//	}
+	
+	int pos=30;
+	int neg=70;
+	
+    @Override public void start(Stage stage) {
+        Scene scene = new Scene(new Group());
+        stage.setTitle("Imported Fruits");
+        stage.setWidth(500);
+        stage.setHeight(500);
+ 
+        int pos=30;
+    	int neg=70;
+        
+        ObservableList<PieChart.Data> pieChartData =
+                FXCollections.observableArrayList(
+                new PieChart.Data("Grapefruit", pos),
+                new PieChart.Data("Oranges", neg),
+               // new PieChart.Data("Plums", 10),
+              //  new PieChart.Data("Pears", 22),
+              /*  new PieChart.Data("Apples", 30) */);
+        final PieChart chart = new PieChart(pieChartData);
+        chart.setTitle("Imported Fruits");
+
+        ((Group) scene.getRoot()).getChildren().add(chart);
+        stage.setScene(scene);
+        stage.show();
+    }
+ 
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
