eea13cb1514b7524bd77f44b49db911bea2224bc
PGiannis <t8180196@aueb.gr>
2021-01-27 14:15:04 +0200

    update class

--- src/Home.java
+++ src/Home.java
@@ -1,13 +1,15 @@
 package src;
 
 import java.awt.Container;
+
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 import javax.swing.*; 
 
-class Home extends JFrame  {
+class Home extends JFrame implements ActionListener  {
 	
 	/* Class variables */
 	
@@ -43,13 +45,15 @@ public Home() {
     option1.setFont(new Font("Arial", Font.PLAIN, 15)); 
     option1.setSize(170, 40); 
     option1.setLocation(360, 100); 
+    option1.addActionListener(this);
     c.add(option1); 
     
     /* Contacts Registration choice method */
     option2 = new JButton("Contacts Registration"); 
     option2.setFont(new Font("Arial", Font.PLAIN, 15)); 
     option2.setSize(170, 40); 
-    option2.setLocation(360, 180); 
+    option2.setLocation(360, 180);
+    option2.addActionListener(this);
     c.add(option2); 
     
     /* Statics choice method */
@@ -57,33 +61,43 @@ public Home() {
     option3.setFont(new Font("Arial", Font.PLAIN, 15)); 
     option3.setSize(170, 40); 
     option3.setLocation(360, 260); 
+    option3.addActionListener(this);
     c.add(option3); 
     
     /* Data Processing choice method */
     option4 = new JButton("Data Processing"); 
     option4.setFont(new Font("Arial", Font.PLAIN, 15)); 
     option4.setSize(170, 40); 
-    option4.setLocation(360, 340); 
+    option4.setLocation(360, 340);
+    option4.addActionListener(this);
     c.add(option4); 
     
     /* Contact Processing choice method */
     option5 = new JButton("Contact Processing"); 
     option5.setFont(new Font("Arial", Font.PLAIN, 15)); 
     option5.setSize(170, 40); 
-    option5.setLocation(360, 420); 
+    option5.setLocation(360, 420);
+    option5.addActionListener(this);
     c.add(option5); 
     
     setVisible(true);
-}
-public void actionPerformed(ActionEvent e) {
-	if  (e.getSource() == option1 || e.getSource() == option2 ) {
-	
-	First_page h = new First_page();
-       	
-	h.show();
-        
-	dispose();
 
-	} 	
 }
+    public void actionPerformed(ActionEvent e) {
+    	if  (e.getSource() == option1 || e.getSource() == option2 ) {
+		
+		Test t = new Test();
+	       	
+		t.show();
+	        
+		dispose();
+	
+    	} else if (e.getSource() == option3) 	{
+    		PieChartPanel p = new PieChartPanel();
+    	}
+    }
+    	
 }
+   
+
+
