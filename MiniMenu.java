40f8a2e1c2fe0ac30d607ce3b9062eab7389a3a3
Ilias Tzinieris <t8180133@aueb.gr>
2021-01-29 00:05:28 +0200

    Mini menu

--- MiniMenu.java
+++ MiniMenu.java
@@ -1,4 +1,3 @@
-
 import java.awt.Container;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
@@ -26,15 +25,15 @@ class  MiniMenu extends JFrame implements ActionListener {
 		
         print = new JButton("Print Data"); 
         print.setFont(new Font("Arial", Font.PLAIN, 15)); 
-        print.setSize(80, 20); 
-        print.setLocation(250, 200); 
+        print.setSize(170, 40); 
+        print.setLocation(200, 200); 
         print.addActionListener(this); 
         c.add(print); 
         
        change = new JButton("Change State"); 
        change.setFont(new Font("Arial", Font.PLAIN, 15)); 
-       change.setSize(80, 20); 
-       change.setLocation(250, 100); 
+       change.setSize(170, 40); 
+       change.setLocation(200, 100); 
        change.addActionListener(this); 
         c.add(change);
         
@@ -46,12 +45,12 @@ class  MiniMenu extends JFrame implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		if(arg0.getSource() == print) {
-			Johny j = new Johny();
+			Data j = new Data();
 			j.show();
 			dispose();
 		}
 		else if (arg0.getSource() ==change) {
-			golfi g = new golfi();
+			Chane_state g = new Change_state();
 			g.show();
 			dispose();
 			
