4c6748a173cdabd5f25f74f57d41c260b88afb22
PGiannis <t8180196@aueb.gr>
2021-01-29 22:02:28 +0200

    last updates

--- src/Positive_Form.java
+++ src/Positive_Form.java
@@ -317,10 +317,10 @@ class Positive_Form extends JFrame implements ActionListener {
 
 			try {
 				boolean error = false;
-				if (firstName2.getText().length() < 5) {
+				if (firstName2.getText().length() < 2) {
 					error = true;
 				}
-				if (lastname2.getText().length() < 5) {
+				if (lastname2.getText().length() < 2) {
 					error = true;
 				}
 
