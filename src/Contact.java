3c6eacb2b5327c26a1b1e4b1f17dc5af358c5b67
PGiannis <t8180196@aueb.gr>
2021-01-29 16:39:07 +0200

    update class

--- src/Contact.java
+++ src/Contact.java
@@ -3,6 +3,9 @@ package src;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Contact {
 
@@ -74,4 +77,82 @@ public class Contact {
 
 		}
 }
+	public static void deleteContact(int contactId) throws Exception {
+		DB db = new DB();
+		Connection con = null;
+		ResultSet rs = null;
+		PreparedStatement stmt = null;
+		String sql1 = "Select * from Contacts where Contact_id=?";
+
+		String sql = "Delete from Contacts where Contact_id=?;";
+		try {
+			
+			con = db.getConnection();
+			
+			stmt = con.prepareStatement(sql1);
+
+			stmt.setInt(1, contactId);
+			
+			rs = stmt.executeQuery();
+			
+			if (!rs.next()) {
+				throw new Exception("Contact not found.");
+			}
+			
+
+			stmt = con.prepareStatement(sql);
+
+			stmt.setInt(1, contactId);
+
+			stmt.executeUpdate();
+
+			stmt.close();
+
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		} finally {
+
+			try {
+				db.close();
+			} catch (Exception e) {
+
+			}
+
+		}
+}
+	
+	public ArrayList<Contact> getPatients() throws Exception {
+
+		ArrayList<Contact> contacts = new ArrayList<Contact>();
+		Connection con = null;
+		DB db = new DB();
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		String slcPatient = "SELECT * FROM Contacts;";
+
+		try {
+			con = db.getConnection();
+			stmt = con.prepareStatement(slcPatient);
+			rs = stmt.executeQuery();
+			while (rs.next()) {
+				 contacts.add(new Contact(rs.getInt("Contact_id"), rs.getString("Name"), rs.getString("Phone"),
+						null));
+			}
+			rs.close();
+			stmt.close();
+			db.close();
+			return contacts;
+
+		} catch (SQLException e) {
+			throw new Exception(e.getMessage());
+			// handle the exception or/and print message
+		} finally {
+
+			try {
+				db.close();
+			} catch (Exception e) {
+				throw new Exception(e.getMessage());
+			}
+		}
+	}
 }
